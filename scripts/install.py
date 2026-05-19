2aac0826357e249a915950a4807b6c690130a39b
serchugar <162186871+serchugar@users.noreply.github.com>
2026-05-19 15:14:52 +0200

    feat(shared, script): add yazi config and improved install script instructions

--- scripts/install.py
+++ scripts/install.py
@@ -8,6 +8,10 @@ OS: str = platform.system()
 REPO: Path = Path(__file__).parent.parent.resolve()
 HOME: Path = Path.home()
 
+SHARED: Path = REPO / "shared"
+LINUX: Path = REPO / "linux"
+WINDOWS: Path = REPO / "windows"
+
 
 def symlink(src: Path, dst: Path) -> None:
     dst.parent.mkdir(parents=True, exist_ok=True)
@@ -29,17 +33,28 @@ def copy(src: Path, dst: Path) -> None:
 
 if __name__ == "__main__":
     print()
-    copy(REPO / "shared/git/.gitconfig", HOME / ".gitconfig")
-    symlink(REPO / "shared/wezterm/.wezterm.lua", HOME / ".wezterm.lua")
+    copy(SHARED / "git/.gitconfig", HOME / ".gitconfig")
+    symlink(SHARED / "wezterm/.wezterm.lua", HOME / ".wezterm.lua")
 
     if OS == "Linux":
         CONFIG: Path = HOME / ".config"
-        symlink(REPO / "shared/alacritty", CONFIG / "alacritty")
+        symlink(SHARED / "alacritty", CONFIG / "alacritty")
+        symlink(SHARED / "yazi", CONFIG / "yazi")
 
     elif OS == "Windows":
         APPDATA: Path = Path(os.environ["APPDATA"])
-        symlink(REPO / "shared/alacritty", APPDATA / "alacritty")
+        symlink(SHARED / "alacritty", APPDATA / "alacritty")
+        symlink(SHARED / "yazi", APPDATA / "yazi/config")
 
     else:
         raise SystemExit(f"OS not supported: {OS}")
+    print("\nThe following programs must be installed manually:\n")
+    print(
+        "\t- Clangd: Copy '.clang-format' and '.clangd' files into the highest "
+        "possible directory within the disk where you are storing your projects"
+    )
+    print(
+        "\t- Powershell: open $PROFILE within a powershell session with your editor "
+        "(E.g: 'nvim $PROFILE') and copy the contents of 'Microsoft.PowerShell_profile.ps1'"
+    )
     print()
